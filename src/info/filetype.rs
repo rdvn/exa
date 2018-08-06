fa2ae1340db1e70dd9fbbb79e085f0b91894ed9c
radovan <rdvn@me.com>
2018-08-06 22:13:10 +0200

    More icons for everyone!

--- src/info/filetype.rs
+++ src/info/filetype.rs
@@ -27,19 +27,15 @@ impl FileExtensions {
         ])
     }
 
-    fn is_image(&self, file: &File) -> bool {
+    fn is_android(&self, file: &File) -> bool {
         file.extension_is_one_of( &[
-            "png", "jpeg", "jpg", "gif", "bmp", "tiff", "tif",
-            "ppm", "pgm", "pbm", "pnm", "webp", "raw", "arw",
-            "svg", "stl", "eps", "dvi", "ps", "cbr",
-            "cbz", "xpm", "ico", "cr2", "orf", "nef",
+            "android", "apk", "gradle",
         ])
     }
 
-    fn is_video(&self, file: &File) -> bool {
+    fn is_apple(&self, file: &File) -> bool {
         file.extension_is_one_of( &[
-            "avi", "flv", "m2v", "mkv", "mov", "mp4", "mpeg",
-            "mpg", "ogm", "ogv", "vob", "wmv", "webm", "m2ts",
+            "apple", "ds_store", "localized",
         ])
     }
 
@@ -56,25 +52,189 @@ impl FileExtensions {
         ])
     }
 
+    fn is_compressed(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+            "zip", "tar", "Z", "z", "gz", "bz2", "a", "ar", "7z",
+            "iso", "dmg", "tc", "rar", "par", "tgz", "xz", "txz",
+            "lzma", "deb", "rpm", "zst",
+        ])
+    }
+    
+    fn is_conf(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+            "conf", "editorconfig",
+        ])
+    }
+    
     fn is_crypto(&self, file: &File) -> bool {
         file.extension_is_one_of( &[
             "asc", "enc", "gpg", "pgp", "sig", "signature", "pfx", "p12",
         ])
     }
 
+    fn is_css(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+            "css", "scss",
+        ])
+    }
+    
+    fn is_doc(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+            "doc", "docx", "gdoc",
+        ])
+    }
+    
+    fn is_ebook(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+            "ebook", "epub", "mobi", "azw",
+        ])
+    }
+    
+    fn is_font(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+            "eot", "otf", "ttf", "woff", "woff2", 
+        ])
+    }
+
+    fn is_git(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+            "git", "gitconfig", "gitignore", "gitignore_global", "gitattributes", 
+        ])
+    }
+    
+    fn is_hs(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+            "hs", "lhs",
+        ])
+    }
+
+    fn is_image(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+            "png", "jpeg", "jpg", "gif", "bmp", "tiff", "tif",
+            "ppm", "pgm", "pbm", "pnm", "webp", "raw", "arw",
+            "svg", "stl", "eps", "dvi", "ps", "cbr", "tiff",
+            "cbz", "xpm", "ico", "cr2", "orf", "nef", "webp",
+            "pxm", "ico",
+        ])
+    }
+
+    fn is_java(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+            "java", "jar", "war",
+        ])
+    }
+
+    fn is_json(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+            "json", "properties",
+        ])
+    }
+
+    fn is_jsx(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+            "jsx", "tsx",
+        ])
+    }
+
+    fn is_md(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+          "md", "license", "markdown", "mkd", "rdoc", "readme",
+        ])
+    }
+    
+    fn is_ppt(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+          "ppt", "gslides", "pptx",
+        ])
+    } 
+    
+    fn is_py(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+          "py", "ipynb", "pyc",
+        ])
+    }  
+    
+    fn is_r(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+          "r", "rdata", "rds",
+        ])
+    }  
+    
+    fn is_rb(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+          "gemfile", "gemspec", "guardfile", "lock", 
+          "procfile", "rakefile", "rspec", "rspec_parallel",
+          "rspec_status", "ru", "rb"
+        ])
+    }
+    
+    fn is_rubydoc(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+          "rubydoc", "erb", "slim",
+        ])
+    }
+    
+    fn is_shell(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+            "shell", "bash", "bash_history", "bash_profile", "bashrc",
+            "fish", "sh", "zsh", "zsh-theme", "zshrc", "zsh_history",
+        ])
+    }
+    
+    fn is_styl(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+          "styl", "stylus",
+        ])
+    }  
+        
+    fn is_tex(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+          "tex", "cls",
+        ])
+    }
+
+    fn is_video(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+            "avi", "flv", "m2v", "mkv", "mov", "mp4", "mpeg",
+            "mpg", "ogm", "ogv", "vob", "wmv", "webm", "m2ts",
+        ])
+    }
+    
+    fn is_vim(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+          "vim", "vimrc", "viminfo"
+        ])
+    }
+    
+    fn is_windows(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+          "bat", "exe", "ini", "com",
+        ])
+    }
+    
+    fn is_xls(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+          "csv", "gsheet", "xlsx",
+        ])
+    }
+    
+    fn is_xml(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+          "xml", "xul",
+        ])
+    }
+ 
+    fn is_yml(&self, file: &File) -> bool {
+        file.extension_is_one_of( &[
+          "yml", "yaml",
+        ])
+    }
+       
     fn is_document(&self, file: &File) -> bool {
         file.extension_is_one_of( &[
             "djvu", "doc", "docx", "dvi", "eml", "eps", "fotd",
             "odp", "odt", "pdf", "ppt", "pptx", "rtf",
             "xls", "xlsx",
-        ])
-    }
-
-    fn is_compressed(&self, file: &File) -> bool {
-        file.extension_is_one_of( &[
-            "zip", "tar", "Z", "z", "gz", "bz2", "a", "ar", "7z",
-            "iso", "dmg", "tc", "rar", "par", "tgz", "xz", "txz",
-            "lzma", "deb", "rpm", "zst",
         ])
     }
 
@@ -122,9 +282,38 @@ impl FileIcon for FileExtensions {
         use output::icons::Icons;
 
         Some(match file {
-            f if self.is_music(f) || self.is_lossless(f) => Icons::Audio.value(),
-            f if self.is_image(f) => Icons::Image.value(),
-            f if self.is_video(f) => Icons::Video.value(),
+            f if self.is_android(f)    => Icons::Android.value(),
+            f if self.is_apple(f)      => Icons::Apple.value(),
+            f if self.is_music(f)      => Icons::Audio.value(),
+            f if self.is_lossless(f)   => Icons::Audio.value(),
+            f if self.is_compressed(f) => Icons::Compressed.value(),
+            f if self.is_conf(f)       => Icons::Conf.value(),
+            f if self.is_crypto(f)     => Icons::Crypto.value(),
+            f if self.is_css(f)        => Icons::Css.value(),
+            f if self.is_doc(f)        => Icons::Doc.value(),
+            f if self.is_ebook(f)      => Icons::Ebook.value(),
+            f if self.is_font(f)       => Icons::Font.value(),
+            f if self.is_git(f)        => Icons::Git.value(),
+            f if self.is_hs(f)         => Icons::Hs.value(),
+            f if self.is_image(f)      => Icons::Image.value(),
+            f if self.is_java(f)       => Icons::Java.value(),
+            f if self.is_json(f)       => Icons::Json.value(),
+            f if self.is_jsx(f)        => Icons::Jsx.value(),
+            f if self.is_md(f)         => Icons::Md.value(),
+            f if self.is_ppt(f)        => Icons::Ppt.value(),
+            f if self.is_py(f)         => Icons::Py.value(),
+            f if self.is_r(f)          => Icons::R.value(),
+            f if self.is_rb(f)         => Icons::Rb.value(),
+            f if self.is_rubydoc(f)    => Icons::Rubydoc.value(),
+            f if self.is_shell(f)      => Icons::Shell.value(),
+            f if self.is_styl(f)       => Icons::Styl.value(),
+            f if self.is_tex(f)        => Icons::Tex.value(),
+            f if self.is_video(f)      => Icons::Video.value(),
+            f if self.is_vim(f)        => Icons::Vim.value(),
+            f if self.is_windows(f)    => Icons::Windows.value(),
+            f if self.is_xls(f)        => Icons::Xls.value(),
+            f if self.is_xml(f)        => Icons::Xml.value(),
+            f if self.is_yml(f)        => Icons::Yml.value(),
             _ => return None,
         })
     }
